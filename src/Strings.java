1b1cb02c4acee15127c832ac1cc90082484ee802
Guilherme Dugaich Oliveira <guidugaich.dev@gmail.com>
2021-11-16 15:05:07 -0300

    string methods

--- src/Strings.java
+++ src/Strings.java
@@ -4,5 +4,15 @@ public class Strings {
         System.out.println(msg.endsWith("rld")); // returns true
         System.out.println(msg.length()); // returns 11
         System.out.println(msg.indexOf("H")); // returns 0
+        System.out.println(msg.replace("Hello", "Goodbye")); // Goodbye World
+
+        // Strings are immutable!
+
+        System.out.println(msg.toLowerCase());
+        System.out.println(msg.toUpperCase());
+
+        System.out.println("Hello \"Guilherme\""); // prints the doulbe quotes
+        System.out.println("C:\\Windows\\Users..."); // prints the backslashes
+        System.out.println("Bom\nDia"); // breaks the line
     }
 }
